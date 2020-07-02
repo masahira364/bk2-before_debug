a8623bd4baff270497ee0ec8648fb84cc8ad458f
vagrant <vagrant@localhost.localdomain>
2020-07-02 05:44:20 +0000

    Add Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_025414) do
+ActiveRecord::Schema.define(version: 2020_07_02_052451) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
