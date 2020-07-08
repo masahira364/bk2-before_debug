4ea2d5137cedd1fa5163f94144dc9b07654c7747
vagrant <vagrant@localhost.localdomain>
2020-07-08 05:51:08 +0000

    fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 	def search
 		@user_or_book = params[:model]
 		@how_search = params[:how]
+		@search = params[:search]
 		if @user_or_book == "1"
 			@users = User.search(params[:search], @user_or_book, @how_search)
 		else
